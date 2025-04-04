4baac98212d3326109a4e5ab6f1a6faf82bf9499
José <rmzspencerjose@outlook.com>
2025-04-04 19:27:31 +0000

    class 4

--- variables-operations.c
+++ variables-operations.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
 int main () {
-    int a[] = "5";
-    int b[] = "9";
+    int width = 5;
+    int length = 9;
 
-    //printf("a*b = %d \n")
+    int X = width*length;
+    int Y = width+width+length+length;
+
+    printf("1. \n");
+    printf("Area = %d \n", X);
+    printf("Perimeter = %d \n", Y);
+
+    int F = 95;
+    int C = (F-32)/1.8;
+
+    printf("2. \n");
+    printf("%d°F = %d°C\n", F, C);
+
+    int min = 349;
+    int hours = min/60;
+    int minutes = 60*5;
+    int result = min-minutes;
+
+    printf("3. \n");
+    printf("%d minutes is equal to %d hours and %d minutes \n", min, hours, result);
 }
