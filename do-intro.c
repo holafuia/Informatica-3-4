10d09dd356c6b7b426d5c6fc08a620650b675613
José <rmzspencerjose@outlook.com>
2025-05-07 16:31:15 +0000

    class 12.1

--- do-intro.c
+++ do-intro.c
@@ -7,15 +7,47 @@ int main(){
     scanf("%d", &number);
 
     int num = 1;
-    int f = number/num;
-    int answer = 1;
+    int answer = 0;
 
     if(number > 0){
-        while(f > 1){
+        while(number/num >= 1){
             num = num*10;
             ++answer;
-        } printf("%d\n", answer);
+        } 
+        printf("%d\n", answer);
     } else {
-        printf("Not a positive number >:(\n");
+        printf("NOT A POSITIVE NUMBER >:(\n");
     }
+
+    //experiment
+    int a;
+
+    printf("enter a positive number: ");
+    scanf("%d", &a);
+
+    float s = 1;
+    float d = 0;
+    int f = a%1;
+    float g = 1;
+    float h = 0;
+
+    if(a > 0){
+        while(a/s >= 1){
+            s = s*10;
+            ++d;
+        } 
+        while(f/g <= 1){
+            g = g/10;
+            ++h;
+        }
+        int j = h+d;
+        printf("%d\n", f);
+        printf("%.0f\n", d);
+        printf("%.0f\n", h);
+        printf("%d\n", j);
+    } else {
+        printf("NOT A POSITIVE NUMBER >:(\n");
+    }
+
+    return 0;
 }
