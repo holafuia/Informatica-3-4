4471bf656321f4f0ff48dadb430d2ac317e593b9
José <rmzspencerjose@outlook.com>
2025-05-09 21:46:48 +0000

    class 14.1

--- exponents.c
+++ exponents.c
@@ -15,21 +15,28 @@ int main(){
     printf("enter the desired loop: ");
     scanf("%d", &form);
 
+    double q = num;
+    double w = exp;
+    double e = pow(q, w);
+    printf("%d ^ %d = %.0lf\n", num, exp, e);
+
     int ran = 1;
     int nums = num;
     int rans;
     int mun = num;
-
+    
     //if (exp != 0){
-        //if(form = 1){
+        //if(form == 1){
         //while(ran < exp){
             //nums = nums * num;
             //++ran;
         //}printf("%d ^ %d = %d\n", num, exp, nums);
-    //}else if(form = 2){
+    //}else if(form == 2){
         //for(rans = 1; rans < exp; ++rans){
             //mun = mun * num;
         //}printf("%d ^ %d = %d\n", num, exp, mun);
+    //}else if(form > 2){
+        //printf("ONE OR TWO, HOW HARD IS THAT >:(\n");
     //}
 //}else if(exp == 0){
     //printf("%d ^ %d = 1\n", num, exp);
