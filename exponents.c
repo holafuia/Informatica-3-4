5cf5452e169ee68b571779f58914abef215c09b8
José <rmzspencerjose@outlook.com>
2025-05-12 16:07:07 +0000

    class 15

--- exponents.c
+++ exponents.c
@@ -5,42 +5,52 @@ int main(){
     int num;
     int exp;
     int form;
+    int loop;
 
     printf("Enter the base number: ");
     scanf("%d", &num);
     printf("Enter the exponent of the base number: ");
     scanf("%d", &exp);
-    printf("while loop = 1\n");
-    printf("for loop = 2\n");
-    printf("enter the desired loop: ");
-    scanf("%d", &form);
+    printf("loop = 1\n");
+    printf("pow = 2\n");
+    printf("enter the desired path: ");
+    scanf("%d", &loop);
+    if(loop == 1){
+        printf("while loop = 1\n");
+        printf("for loop = 2\n");
+        printf("enter the desired loop: ");
+        scanf("%d", &form);
+    }
 
     double q = num;
     double w = exp;
-    double e = pow(q, w);
-    printf("%d ^ %d = %.0lf\n", num, exp, e);
 
     int ran = 1;
     int nums = num;
     int rans;
     int mun = num;
     
-    //if (exp != 0){
-        //if(form == 1){
-        //while(ran < exp){
-            //nums = nums * num;
-            //++ran;
-        //}printf("%d ^ %d = %d\n", num, exp, nums);
-    //}else if(form == 2){
-        //for(rans = 1; rans < exp; ++rans){
-            //mun = mun * num;
-        //}printf("%d ^ %d = %d\n", num, exp, mun);
-    //}else if(form > 2){
-        //printf("ONE OR TWO, HOW HARD IS THAT >:(\n");
-    //}
-//}else if(exp == 0){
-    //printf("%d ^ %d = 1\n", num, exp);
-//}
+    if(loop == 1){
+        if (exp != 0){
+            if(form == 1){
+                while(ran < exp){
+                nums = nums * num;
+                ++ran;
+            }printf("%d ^ %d = %d\n", num, exp, nums);
+        }else if(form == 2){
+            for(rans = 1; rans < exp; ++rans){
+                mun = mun * num;
+            }printf("%d ^ %d = %d\n", num, exp, mun);
+        }else if(form > 2){
+            printf("ONE OR TWO, HOW HARD IS THAT >:(\n");
+        }
+        }else if(exp == 0){
+        printf("%d ^ %d = 1\n", num, exp);
+        }
+    }else if(loop == 2){
+        double e = pow(q, w);
+        printf("%d ^ %d = %.0lf\n", num, exp, e);
+    }
 
 return 0;
 }
